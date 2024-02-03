8d263a20350c0748b64a9b4f8784e228b27b702b
“Micaela <mseminara@2innovation.it>
2024-02-03 14:48:15 +0100

    examples

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,16 @@
+import 'package:common/models/example.dart';
+
 void main(List<String> args) {
-  print('hello world');
+  
+  //testing the common class Person
+
+  final person = Person(firstName: 'John', lastName: 'Connor', age: 21);
+
+  print('hello ${person.firstName}');
+  print(person.toJson());
+
+  final test = {"firstName": "Marie", "lastName": "Curie", "age": 30};
+
+  final Person newPerson = Person.fromJson(test);
+  print(newPerson.toJson());
 }
